e7651d6441724ecc95574bb5d3faf5e99153c343
Suyog241005 <suyoghabbu@gmail.com>
2026-03-09 15:02:10 +0530

    added previou snapshots of the weekly downloads

--- components/metrics/trends-chart.tsx
+++ components/metrics/trends-chart.tsx
@@ -162,6 +162,7 @@ export const TrendsChart = () => {
             },
           },
           y: {
+            type:"logarithmic",
             grid: {
               color: gridColor,
             },
